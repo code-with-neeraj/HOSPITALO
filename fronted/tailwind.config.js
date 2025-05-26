89d8547b4fc64152d1b6cd68de628041b880b479
code-with-neeraj <neerajkr145518@gmail.com>
2025-05-26 10:50:54 +0530

    first commit

---
+++ fronted/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+    
+  theme: {
+    extend: {
+      colors: {
+       'primary': '#5f6FFF', // Example primary color
+      }
+    },
+  },
+  plugins:[],
+}
+ 
