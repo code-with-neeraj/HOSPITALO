6db997e2bc782bfe18872292502c99c39091b74e
code-with-neeraj <neerajkr145518@gmail.com>
2025-06-21 21:30:14 +0530

    add function login logout for user

--- backend/controllers/doctorControler.js
+++ backend/controllers/doctorControler.js
@@ -15,8 +15,19 @@ const changeAvailablity = async (req, res) => {
         console.log(error);
         res.json({ success: false, message: error.message })
     }
-
-
 }
 
-export {changeAvailablity}
+const doctorList = async (req, res) => {
+    try {
+        
+        const doctors = await doctorModel.find({}).select(['-password','-email'])
+
+        res.json({success:true,doctors})
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export {changeAvailablity,doctorList}
