f4e087c7bd88e225f9e3bd4e23ec74086f8170d7
code-with-neeraj <neerajkr145518@gmail.com>
2025-06-23 16:39:44 +0530

    myprofile page complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -87,7 +87,7 @@ const loginUser = async (req, res) => {
 const getProfile = async (req, res) => {
 
     try {
-        const { userId } = req.body
+        const  {userId}  = req.body
         const userData = await userModel.findById(userId).select('-password')
 
         res.json({ success: true, userData })
@@ -133,7 +133,7 @@ const updateProfile = async (req, res) => {
 const bookAppointment = async (req, res) => {
 
     try {
-
+        
         const { userId, docId, slotDate, slotTime } = req.body
 
         const docData = await doctorModel.findById(docId).select('-password')
@@ -185,9 +185,63 @@ const bookAppointment = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 
+}
+
+// API to get user appointments for frontend my-appointments page
+const listAppintment = async (req, res) => {
+
+    try {
+
+        const {userId} = req.body
+        const appointments = await appointmentModel.find({userId})
+
+        res.json({success:true,appointments})
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
-export { registerUser, loginUser, getProfile, updateProfile, bookAppointment }
+// API to cencel appointment
+const cancelAppointment = async (req, res) => {
+
+    try {
+
+        const {userId, appointmentId} = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        // verify appointment user
+        if (appointmentData.userId !== userId) {
+            return res.json({success:false,message:'Unauthorized action'})
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId, {cancelled:true})
+
+        // releasing doctor slot
+
+        const {docId, slotDate, slotTime} = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+        res.json({success:true, message:'Appointment Cancelled'})
+
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
+}
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppintment, cancelAppointment }
 
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { bookAppointment, getProfile, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
+import { bookAppointment, cancelAppointment, getProfile, listAppintment, loginUser, registerUser, updateProfile } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 
@@ -12,6 +12,9 @@ userRouter.post('/login',loginUser)
 userRouter.get('/get-profile', authUser, getProfile)
 userRouter.post('/update-profile', upload.single('image') ,authUser, updateProfile)
 userRouter.post('/book-appointment',authUser,bookAppointment)
+userRouter.get('/appointments',authUser,listAppintment)
+userRouter.post('/cancel-appointment',authUser,cancelAppointment)
+
 
 
 
