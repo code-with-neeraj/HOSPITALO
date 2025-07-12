3222949ab0e49e9a95097a88b8e7ef74551e3f84
code-with-neeraj <neerajkr145518@gmail.com>
2025-07-12 10:56:19 +0530

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { bookAppointment, cancelAppointment, getProfile, listAppintment, loginUser, logout, paymentRazorpay, registerUser, resetPassword, sendResetOtp,  updateProfile,  verifyRazorpay } from '../controllers/userController.js'
+import { bookAppointment, cancelAppointment, getProfile, listAppintment, loginUser, logout, paymentRazorpay, registerUser, resetPassword, sendResetOtp,  updateProfile,  verifyRazorpay, verifyResetOtp } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 
@@ -12,7 +12,12 @@ userRouter.post('/logout',logout)
 
 
 userRouter.post('/send-reset-otp', sendResetOtp);
+userRouter.post('/verify-reset-otp', verifyResetOtp);
 userRouter.post('/reset-password', resetPassword);
+
+
+
+
 
 userRouter.get('/get-profile', authUser, getProfile)
 userRouter.post('/update-profile', upload.single('image') ,authUser, updateProfile)
